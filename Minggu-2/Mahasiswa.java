ccf03ca10e164595988f27c5a1a66e765aadb810
aryvillanda48 <aryvillanda48@gmail.com>
2017-02-28 19:37:39 +0700

    Add files via upload

---
+++ Minggu-2/Mahasiswa.java
@@ -0,0 +1,9 @@
+/*program Mahasiswa*/
+
+class Mahasiswa {
+     public static void main(String[] argv) {
+          System.out.println("Nama	: Ary Villanda Dwitama");
+		  System.out.println("NIM	: 10115280");
+		  System.out.println("Alamat  : Kp. Nambo RT 006/RW 002 Desa Batukarut Kec. Arjasari Kab. Bandung");
+     }
+}
