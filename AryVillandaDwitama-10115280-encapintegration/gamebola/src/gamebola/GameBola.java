a1295b9f9a6e8676de93bbd74e8ad6ec3e2fc624
aryvillanda48 <aryvillanda48@gmail.com>
2017-05-17 22:35:44 +0700

    Add files via upload

---
+++ AryVillandaDwitama-10115280-encapintegration/gamebola/src/gamebola/GameBola.java
@@ -0,0 +1,69 @@
+package gamebola;
+
+
+public class GameBola {
+
+    private double harga;
+    private String tahunrilis;
+    private String pengembang;
+    private String nama;
+    private String platform;  
+    private String engine; 
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public String getTahunrilis() {
+        return tahunrilis;
+    }
+
+    public void setTahunrilis(String tahunrilis) {
+        this.tahunrilis = tahunrilis;
+    }
+
+    public String getPengembang() {
+        return pengembang;
+    }
+
+    public void setPengembang(String pengembang) {
+        this.pengembang = pengembang;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    void setTahunrilis() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void setHarga() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+}
