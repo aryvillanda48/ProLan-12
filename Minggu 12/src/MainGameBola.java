4cbcdbf8fa79d08756c5f3a0508aa92a020560b8
aryvillanda48 <aryvillanda48@gmail.com>
2017-05-30 22:36:50 +0700

    Add files via upload

---
+++ Minggu 12/src/MainGameBola.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+public class MainGameBola {
+ 
+    public static void main(String[] args) {
+    GameBolaImpl<String> bola = new GameBolaImpl<>();
+    
+    Scanner input = new Scanner(System.in);
+    int n;
+    
+    do {
+    System.out.println("Menu");
+    System.out.println("1. PES 2016");
+    System.out.println("2. PES 2017");
+    System.out.println("3. FIFA 17");
+    System.out.println("4. KELUAR");
+    
+    System.out.print("Masukan Pilihan Anda : ");
+    n = input.nextInt();
+    
+    if(n == 1){
+        bola.setNama("PES 2016");
+        System.out.println("Nama           : "+bola.getNama());
+        bola.setPengembang("KONAMI");
+        System.out.println("Developer      : "+bola.getPengembang());
+        bola.setPlatform("PC, PS4, PS3, Xbox One, dan Xbox 360");
+        System.out.println("Platform       : "+bola.getPlatform());
+        bola.setEngine("Fox Engine");
+        System.out.println("Engine         : "+bola.getEngine());
+        bola.setTahunrilis("22 September 2015");
+        System.out.println("Tahun Rilis    : "+bola.getTahunrilis());
+        System.out.println("Harga Sekarang : +-Rp."+bola.getHarga1());
+    }
+    if(n == 2){
+        bola.setNama("PES 2017");
+        System.out.println("Nama           : "+bola.getNama());
+        bola.setPengembang("KONAMI");
+        System.out.println("Developer      : "+bola.getPengembang());
+        bola.setPlatform("PC, PS4, PS3, Xbox One, dan Xbox 360");
+        System.out.println("Platform       : "+bola.getPlatform());
+        bola.setEngine("Fox Engine");
+        System.out.println("Engine         : "+bola.getEngine());
+        bola.setTahunrilis("15 September 2016");
+        System.out.println("Tahun Rilis    : "+bola.getTahunrilis());
+        System.out.println("Harga Sekarang : +-Rp."+bola.getHarga2());
+    }
+    if(n == 3){
+        bola.setNama("FIFA 17");
+        System.out.println("Nama           : "+bola.getNama());
+        bola.setPengembang("EA Sports");
+        System.out.println("Developer      : "+bola.getPengembang());
+        bola.setPlatform("PC, PS4, PS3, Xbox One, dan Xbox 360");
+        System.out.println("Platform       : "+bola.getPlatform());
+        bola.setEngine("Frostbite 3");
+        System.out.println("Engine         : "+bola.getEngine());
+        bola.setTahunrilis("29 September 2016");
+        System.out.println("Tahun Rilis    : "+bola.getTahunrilis());
+        System.out.println("Harga Sekarang : +-Rp."+bola.getHarga3());
+    }
+    }while(n!=4);
+    }
+}
---
+++ Minggu 12/test/GameBolaImplTest.java
@@ -0,0 +1,216 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Nishikino Ary
+ */
+public class GameBolaImplTest {
+    
+    public GameBolaImplTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getHarga1 method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetHarga1() {
+        System.out.println("getHarga1");
+        GameBolaImpl instance = new GameBolaImpl();
+        double expResult = 0.0;
+        double result = instance.getHarga1();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getHarga2 method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetHarga2() {
+        System.out.println("getHarga2");
+        GameBolaImpl instance = new GameBolaImpl();
+        double expResult = 0.0;
+        double result = instance.getHarga2();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getHarga3 method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetHarga3() {
+        System.out.println("getHarga3");
+        GameBolaImpl instance = new GameBolaImpl();
+        double expResult = 0.0;
+        double result = instance.getHarga3();
+        assertEquals(expResult, result, 0.0);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getTahunrilis method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetTahunrilis() {
+        System.out.println("getTahunrilis");
+        GameBolaImpl instance = new GameBolaImpl();
+        Object expResult = null;
+        Object result = instance.getTahunrilis();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setTahunrilis method, of class GameBolaImpl.
+     */
+    @Test
+    public void testSetTahunrilis() {
+        System.out.println("setTahunrilis");
+        Object tahunrilis = null;
+        GameBolaImpl instance = new GameBolaImpl();
+        instance.setTahunrilis(tahunrilis);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPengembang method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetPengembang() {
+        System.out.println("getPengembang");
+        GameBolaImpl instance = new GameBolaImpl();
+        Object expResult = null;
+        Object result = instance.getPengembang();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPengembang method, of class GameBolaImpl.
+     */
+    @Test
+    public void testSetPengembang() {
+        System.out.println("setPengembang");
+        Object pengembang = null;
+        GameBolaImpl instance = new GameBolaImpl();
+        instance.setPengembang(pengembang);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getNama method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetNama() {
+        System.out.println("getNama");
+        GameBolaImpl instance = new GameBolaImpl();
+        Object expResult = null;
+        Object result = instance.getNama();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setNama method, of class GameBolaImpl.
+     */
+    @Test
+    public void testSetNama() {
+        System.out.println("setNama");
+        Object nama = null;
+        GameBolaImpl instance = new GameBolaImpl();
+        instance.setNama(nama);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getPlatform method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetPlatform() {
+        System.out.println("getPlatform");
+        GameBolaImpl instance = new GameBolaImpl();
+        Object expResult = null;
+        Object result = instance.getPlatform();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setPlatform method, of class GameBolaImpl.
+     */
+    @Test
+    public void testSetPlatform() {
+        System.out.println("setPlatform");
+        Object platform = null;
+        GameBolaImpl instance = new GameBolaImpl();
+        instance.setPlatform(platform);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEngine method, of class GameBolaImpl.
+     */
+    @Test
+    public void testGetEngine() {
+        System.out.println("getEngine");
+        GameBolaImpl instance = new GameBolaImpl();
+        Object expResult = null;
+        Object result = instance.getEngine();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setEngine method, of class GameBolaImpl.
+     */
+    @Test
+    public void testSetEngine() {
+        System.out.println("setEngine");
+        Object engine = null;
+        GameBolaImpl instance = new GameBolaImpl();
+        instance.setEngine(engine);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
